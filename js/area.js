9a2257f16c62bae4c7d33d55b2ca09770d7fdd92
barkatzx <barkatullah.zx@gmail.com>
2023-02-18 15:58:57 +0600

    Function Declare

---
+++ js/area.js
@@ -0,0 +1,29 @@
+function calculateTriangleArea() {
+    const input1Value = parseFloat(document.getElementById("triangle-field-b").value);
+    const input2Value = parseFloat(document.getElementById("triangle-field-h").value);
+  
+    if (isNaN(input1Value) || isNaN(input2Value)) {
+      alert("Invalid input! Please enter a valid number.");
+      return;
+    }
+  
+    document.getElementById("triangle-b").innerText = input1Value;
+    document.getElementById("triangle-h").innerText = input2Value;
+    const mainName = document.getElementById("triangle-name").innerText;
+  
+    const total = 0.5 * input1Value * input2Value;
+    console.log(typeof(total));
+  
+    const container = document.getElementById('table-container');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${1}</td>
+    <td>${mainName}</td>
+    <td>${total}</td>
+    <td> <button class="btn-primary rounded-btn">convert to m<sup>2</sup></button> </td>
+    `
+    container.appendChild(tr);
+  }
+  
+document.getElementById('triangle-btn').addEventListener("click", calculateTriangleArea);
+  
--- js/geometry.js
+++ js/geometry.js
@@ -5,9 +5,17 @@ document.getElementById('triangle-btn').addEventListener("click", function() {
 
   document.getElementById("triangle-b").innerText = input1Value;
   document.getElementById("triangle-h").innerText = input2Value;
+  const mainName = document.getElementById("triangle-name").innerText;
 
   const total = 0.5 * input1Value * input2Value;
-  
-  const paragraphElement = document.getElementById("total");
-  paragraphElement.innerHTML = (total);
+
+  const container = document.getElementById('table-container');
+  const tr = document.createElement('tr');
+  tr.innerHTML = `
+  <td>${1}</td>
+  <td>${mainName}</td>
+  <td>${total}</td>
+  <td> <button class="btn-primary rounded-btn">convert to m<sup>2</sup></button> </td>
+  `
+  container.appendChild(tr);
 })
