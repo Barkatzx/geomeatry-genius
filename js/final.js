9fabd116ac13ea30810506fac5043f4d3e83b649
barkatzx <barkatullah.zx@gmail.com>
2023-02-18 18:10:42 +0600

    Four Card Function Added

--- js/final.js
+++ js/final.js
@@ -65,6 +65,49 @@ document.getElementById('parallelogram-btn').addEventListener("click", function(
 })
 
 
+// Fourth Card Function
+document.getElementById('rhombus-btn').addEventListener("click", function() {
+    serial += 1;
+    const input1Value = parseFloat(document.getElementById("rhombus-field1").value);
+    const input2Value = parseFloat(document.getElementById("rhombus-field2").value);
+
+    if (isNaN(input1Value) || isNaN(input2Value)) {
+        alert("Invalid input! Please enter a valid number.");
+        return;
+      }
+  
+    document.getElementById("rhombus-d1").innerText = input1Value;
+    document.getElementById("rhombus-d2").innerText = input2Value;
+    const mainName = document.getElementById("rhombus-name").innerText;
+  
+    const total = 0.5 * input1Value * input2Value;
+    const totalValue = total.toFixed(2);
+
+    displayData(mainName, totalValue);
+})
+
+// Five Card Function
+document.getElementById('rhombus-btn').addEventListener("click", function() {
+    serial += 1;
+    const input1Value = parseFloat(document.getElementById("rhombus-field1").value);
+    const input2Value = parseFloat(document.getElementById("rhombus-field2").value);
+
+    if (isNaN(input1Value) || isNaN(input2Value)) {
+        alert("Invalid input! Please enter a valid number.");
+        return;
+      }
+  
+    document.getElementById("rhombus-d1").innerText = input1Value;
+    document.getElementById("rhombus-d2").innerText = input2Value;
+    const mainName = document.getElementById("rhombus-name").innerText;
+  
+    const total = 0.5 * input1Value * input2Value;
+    const totalValue = total.toFixed(2);
+
+    displayData(mainName, totalValue);
+})
+
+
 
 // Function Declare For Aria Calculation
 function displayData(mainName, totalValue){
